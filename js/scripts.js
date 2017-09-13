5e8b78cadb9b012e978e293969f891588ed84331
sami-mai <samirah.maison@gmail>
2017-09-13 21:26:32 +0000

    add completed lesson

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+//   $("button#green").click(function() {
+//     $("body").addClass("green-background");
+//   });
+//
+//   $("button#yellow").click(function() {
+//     $("body").addClass("yellow-background");
+//   });
+//
+//   $("button#red").click(function() {
+//     $("body").addClass("red-background");
+//   });
+// });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+
+// $("button#green").click(function() {
+//   $("body").css("background-color", "green");
+// });
